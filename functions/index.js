c85b33939b704baffb1ecb9bb14c2b3025d39b01
schkiho <schkiho@yahoo.de>
2020-07-10 16:06:37 +0200

    setup firebase &  html, creating and deploying a function, callable functions

---
+++ functions/index.js
@@ -0,0 +1,20 @@
+const functions = require("firebase-functions");
+
+// http request 1
+exports.randomNumber = functions.https.onRequest((request, response) => {
+  const number = Math.round(Math.random() * 100);
+  console.log(number);
+  response.send(number.toString());
+});
+
+// http request 2
+exports.toTheDojo = functions.https.onRequest((request, response) => {
+  const number = Math.round(Math.random() * 100);
+  response.redirect("https://www.thenetninja.co.uk");
+});
+
+// http callable function
+exports.sayHello = functions.https.onCall((data, context) => {
+  const name = data.name;
+  return `Hello, ${name}`;
+});
---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+const requestModal = document.querySelector(".new-request");
+const requestLink = document.querySelector(".add-request");
+
+// open request modal
+requestLink.addEventListener("click", () => {
+  requestModal.classList.add("open");
+});
+
+// close request modal
+requestModal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("new-request")) {
+    requestModal.classList.remove("open");
+  }
+});
+
+// sayHello function call
+const button = document.querySelector(".call");
+button.addEventListener("click", () => {
+  // get function reference
+  const sayHello = firebase.functions().httpsCallable("sayHello");
+  sayHello({ name: "shaun" }).then((result) => {
+    console.log(result.data);
+  });
+});
