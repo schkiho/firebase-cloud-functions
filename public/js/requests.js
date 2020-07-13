957c93b1f29eb3dc71c22016727784381608eb5f
schkiho <schkiho@yahoo.de>
2020-07-13 15:35:32 +0200

    adding Vue instance

--- public/js/requests.js
+++ public/js/requests.js
@@ -1,9 +1,17 @@
-const ref = firebase.firestore().collection("requests");
+let app = new Vue({
+  el: "#app",
+  data: {
+    requests: [],
+  },
+  mounted() {
+    const ref = firebase.firestore().collection("requests");
 
-ref.onSnapshot((snapshot) => {
-  let requests = [];
-  snapshot.forEach((doc) => {
-    requests.push({ ...doc.data(), id: doc.id });
-  });
-  console.log(requests);
+    ref.onSnapshot((snapshot) => {
+      let requests = [];
+      snapshot.forEach((doc) => {
+        requests.push({ ...doc.data(), id: doc.id });
+      });
+      this.requests = requests;
+    });
+  },
 });
